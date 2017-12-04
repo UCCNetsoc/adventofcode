eaffc09b1d04e26f9b6a3672fa5a9120e40c19df
Adam Gillessen <adamgillessen@gmail.com>
2017-12-04 18:53:16 +0000

    Added python solution to day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,7 @@
+from collections import Counter
+
+
+
 def day1():
     with open("d4.in") as f:
         valid = 0
@@ -12,6 +16,21 @@ def day1():
         print(valid)
 
 
+def day2():
+    with open("d4.in") as f:
+        valid = 0
+        for line in f:
+            seen = []
+            for phrase in line.strip().split():
+                bag = Counter(phrase)
+                try: seen.remove(bag)
+                except: seen.append(bag)
+                else: break
+            else:
+                valid += 1
+        print(valid)
+
 
 if __name__ == "__main__":
     day1()
+    day2()
