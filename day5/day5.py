55cc655c54de30dcfbb87e7a97f6f5e378db1b05
Adam Gillessen <adamgillessen@gmail.com>
2017-12-05 12:28:27 +0000

    Added a python solution to day 5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,15 @@
+def part1():
+    with open("d5.in") as f:
+        ins = [int(l.strip()) for l in f]
+        ins.append(None)
+
+        i = 0
+        jumps = 0
+        while ins[i] is not None:
+            ins[i], i = ins[i]+1, i+ins[i]
+            jumps += 1
+        print(jumps)
+
+
+if __name__ == "__main__":
+    part1()
