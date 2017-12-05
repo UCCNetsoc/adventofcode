f4c623cad08d8eb0b095c1944f27a6bff04b62c5
Adam Gillessen <adamgillessen@gmail.com>
2017-12-05 12:45:03 +0000

    Added a Golang solution to day 5 part 2

--- day5/day5.go
+++ day5/day5.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func main() {
+func part1() {
 	f, err := os.Open("d5.in")
 	if err != nil {
 		panic(err)
@@ -33,4 +33,41 @@ func main() {
 		jumps++
 	}
 	fmt.Println(jumps)
+}
+
+func part2() {
+	f, err := os.Open("d5.in")
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		ins []int
+		sc  = bufio.NewScanner(f)
+	)
+	for sc.Scan() {
+		offset, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			panic(err)
+		}
+		ins = append(ins, offset)
+	}
+
+	var jumps, i int
+	for i != len(ins) {
+		off := ins[i]
+		if off >= 3 {
+			ins[i]--
+		} else {
+			ins[i]++
+		}
+		i += off
+		jumps++
+	}
+	fmt.Println(jumps)
+}
+
+func main() {
+	part1()
+	part2()
 }
