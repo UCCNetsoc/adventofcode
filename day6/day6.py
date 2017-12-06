f6cedce9cfe99310c9bb35bb07cd2493bf532a6b
Adam Gillessen <adamgillessen@gmail.com>
2017-12-06 11:12:42 +0000

    Added a python solution to day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -28,5 +28,36 @@ def part1():
     print(redistributions)
 
 
+def part2():
+    with open("d6.in") as f:
+        mem = list(map(int, f.read().strip().split()))
+    
+    seen_configs = {}
+    redistributions = 0
+    while True:
+        seen_configs[hash(tuple(mem))] = redistributions
+
+        max_n, max_i = float("-inf"), -1
+        for i, n in enumerate(mem):
+            if n > max_n:
+                max_n = n
+                max_i = i
+
+        to_alloc = mem[max_i]
+        mem[max_i] = 0
+        inc = lambda i: (i+1)%len(mem)
+        i = inc(max_i)
+        while to_alloc > 0:
+            mem[i] += 1
+            to_alloc -= 1
+            i = inc(i)
+        redistributions += 1
+
+        if hash(tuple(mem)) in seen_configs:
+            break
+    print(redistributions - seen_configs[hash(tuple(mem))])
+
+
 if __name__ == "__main__":
     part1()
+    part2()
